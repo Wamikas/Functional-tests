d349fd247e5845ef941b5c69958cf7def2d7dba2
wamikas <wamikas@thoughtworks.com>
2016-01-30 14:01:00 +0530

    Adding sample files

---
+++ src/main/java/pages/basePage.java
@@ -0,0 +1,7 @@
+package pages;
+
+/**
+ * Created by wamika on 1/30/16.
+ */
+public class basePage {
+}
---
+++ src/main/java/setup/testSetup.java
@@ -0,0 +1,7 @@
+package setup;
+
+/**
+ * Created by wamika on 1/30/16.
+ */
+public class testSetup {
+}
